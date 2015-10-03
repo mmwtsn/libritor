841e74048c99e85a70e8e12b4f923190b43fffb6
M. Maxwell Watson <hi@mmwtsn.com>
2015-10-02 23:11:52 -0400

    Add, test repos() wrapper function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-export default {}
+/**
+ * Module dependencies.
+ */
+import GitHub from 'github'
+
+export default {
+  /**
+   * Gets all repositories from an organization asynchronously.
+   *
+   * @param {string} org - Name of the organization.
+   * @param {function} callback - Callback following the err, data pattern.
+   * @returns {string[]}
+   * @public
+   */
+  repos (org, callback) {
+    const github = new GitHub({ version: '3.0.0' })
+
+    github.repos.getFromOrg({ org }, (err, data) => {
+      if (err) return callback(err)
+
+      return callback(null, data)
+    })
+  }
+}
---
+++ test/integration/index.js
@@ -0,0 +1,28 @@
+/* global describe, it */
+
+import assert from 'assert'
+import nock from 'nock'
+import index from '../../src'
+
+// Mock reply from GitHub API v3
+const reply = JSON.stringify([
+  { name: 'a', date: 20150101, thing: true },
+  { name: 'b', date: 20150102, thing: true }
+])
+
+// Prevent outbound HTTP requests
+nock('https://api.github.com')
+  .get('/orgs/attn/repos')
+  .reply(200, reply)
+
+describe('repos', () => {
+  it('returns an array of repository names', done => {
+    index.repos('attn', (err, data) => {
+      if (err) throw new Error(err)
+
+      assert.deepEqual(data, reply)
+
+      done()
+    })
+  })
+})
