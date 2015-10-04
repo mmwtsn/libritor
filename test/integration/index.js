5dc7765353101109e33dec28a7a1ed67bad54099
M. Maxwell Watson <hi@mmwtsn.com>
2015-10-03 23:43:10 -0400

    Move Nock response inline

--- test/integration/index.js
+++ test/integration/index.js
@@ -4,16 +4,13 @@ import assert from 'assert'
 import nock from 'nock'
 import index from '../../src'
 
-// Mock reply from GitHub API v3
-const reply = JSON.stringify([
-  { name: 'a', date: 20150101, thing: true },
-  { name: 'b', date: 20150102, thing: true }
-])
-
 // Prevent outbound HTTP requests
 nock('https://api.github.com')
   .get('/orgs/attn/repos')
-  .reply(200, reply)
+  .reply(200, JSON.stringify([
+    { name: 'a', date: 20150101, thing: true },
+    { name: 'b', date: 20150102, thing: true }
+  ]))
 
 describe('repos', () => {
   it('returns an array of repository names', done => {
